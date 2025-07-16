c18fa53a22348a99d9b54018813a4690099c5143
SOLSTICE <mbenane@ABM-FFSDFS3.localdomain>
2025-07-16 17:10:26 +0200

    Structure fichiers Arduino mise en place (Sensor, Pump, OLED, Serial)

---
+++ arduino_scad/arduino_scad.ino
@@ -0,0 +1,14 @@
+#include "sensor.h"
+#include "oled.h"
+#include "pump.h"
+#include "serial_comm.h"
+
+
+void setup() {
+  Serail.begin(9600);
+
+}
+
+void loop() {
+  
+}
---
+++ arduino_scad/oled.h
@@ -0,0 +1,6 @@
+
+// === Paramètres pour l'écran OLED ===
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
